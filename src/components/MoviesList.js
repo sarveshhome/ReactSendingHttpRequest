43cb4640290f0185eedff50b562982075fea2675
sarveshhome <sarvesh.carlton@gmail.com>
2021-05-23 08:49:40 +0100

    http error handing url change

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -7,7 +7,8 @@ const MovieList = (props) => {
   return (
     <ul className={classes['movies-list']}>
       {props.movies.map((movie) => (
-        <Movie        
+        <Movie  
+          key={movie.id}      
           title={movie.title}
           releaseDate={movie.releaseDate}
           openingText={movie.openingText}
